14bb3ec128214e9bdaa7cc84d4d712d44dd8ab03
fatema <fatema@0e778a63-011e-455f-b6d2-55bfd8c9ab11>
2018-06-04 10:27:16 +0000

    Structure of Route and RoutPoint are done

---
+++ OnlineBusTicketManagement/OBTM.Core/Interfaces/IRouteRepository.cs
@@ -0,0 +1,13 @@
+﻿using OBTM.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBTM.Core.Interfaces
+{
+    public interface IRouteRepository: IGenericRepository<Route>
+    {
+    }
+}
