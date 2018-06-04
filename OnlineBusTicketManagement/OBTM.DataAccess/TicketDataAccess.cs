5a2acb73bbd6a30e56ada8acb9bea586bc7e38b2
foysal <foysal@0e778a63-011e-455f-b6d2-55bfd8c9ab11>
2018-06-04 10:23:52 +0000

    added TicketDataAccess in OBTM.DataAccess

---
+++ OnlineBusTicketManagement/OBTM.DataAccess/TicketDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using OBTM.Core.Interfaces;
+using OBTM.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBTM.DataAccess
+{
+    public class TicketDataAccess : GenericDataAccess<Ticket>, ITicketRepository
+    {
+        public TicketDataAccess(OBTMDbContext context) : base(context)
+        {
+
+        }
+    }
+}
