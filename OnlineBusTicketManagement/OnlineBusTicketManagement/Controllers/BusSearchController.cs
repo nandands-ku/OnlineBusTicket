e917e314afeb9cbc802eb12d16f6059c20c69da8
Nandan.rupok <Nandan.rupok@0e778a63-011e-455f-b6d2-55bfd8c9ab11>
2018-06-06 08:18:04 +0000

    BusSearchController Added

---
+++ OnlineBusTicketManagement/OnlineBusTicketManagement/Controllers/BusSearchController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineBusTicketManagement.Controllers
+{
+    public class BusSearchController : Controller
+    {
+        // GET: BusSearch
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewSeat()
+        {
+            return View();
+        }
+    }
+}
