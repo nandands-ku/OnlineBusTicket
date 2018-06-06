9631b9d41d70fcc8b1a7d0269a42c3f338181826
fatema <fatema@0e778a63-011e-455f-b6d2-55bfd8c9ab11>
2018-06-06 04:58:22 +0000

    layout has been added

---
+++ OnlineBusTicketManagement/OnlineBusTicketManagement/Controllers/RouteController.cs
@@ -0,0 +1,31 @@
+﻿using OBTM.Core.Models;
+using OBTM.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineBusTicketManagement.Controllers
+{
+    public class RouteController : Controller
+    {
+        // GET: Route
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SaveRoute(Route r)
+        {
+            RouteService rs = new RouteService();
+            rs.Save(r);
+            return View("Index");
+        }
+        public ActionResult DeleteRoute(int id)
+        {
+            RouteService rs = new RouteService();
+            rs.Delete(id);
+            return View("Index");
+        }
+    }
+}
